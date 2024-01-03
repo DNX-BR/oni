f50e95ce4458275a0f01e4c6b2cb9e72e8c4743c
Gabriela Maximino <39780413+gabrielamaximino@users.noreply.github.com>
2024-01-03 12:31:19 -0300

    Ajustes na feature de build args

--- src/docker.js
+++ src/docker.js
@@ -25,13 +25,15 @@ async function BuildImageBuildKit(
                                   
     let args = '';
     
-    if (buildArgs.length > 0) {
-      if (typeof buildArgs === 'string') {
-        // If buildArgs is a string, split it into a list based on a delimiter (e.g., comma)
-        buildArgs = buildArgs.split(',');
-      }
-      for (const arg of buildArgs) {
-        args += `--opt build-arg:${arg} `;
+    if (buildArgs) {
+      if (buildArgs.length > 0) {
+        if (typeof buildArgs === 'string') {
+          // If buildArgs is a string, split it into a list based on a delimiter (e.g., comma)
+          buildArgs = buildArgs.split(',');
+        }
+        for (const arg of buildArgs) {
+          args += `--opt build-arg:${arg} `;
+        }
       }
     }
 
